11798c52607d75fb774ec73afbff6c150bc10bb4
FREEWING.JP <freewing.jp@gmail.com>
2017-11-05 09:00:39 +0900

    improve install bash script

--- raspberry_pi_2.1.1/phantomjs_for_raspbian.sh
+++ raspberry_pi_2.1.1/phantomjs_for_raspbian.sh
@@ -2,11 +2,11 @@
 
 mkdir ~/phantomjs_tmp
 cd ~/phantomjs_tmp
-git clone https://github.com/FREEWING-JP/phantomjs.git -b feature/build_raspberry_pi_2.1.1 --single-branch --depth=1
-cd ./phantomjs/raspberry_pi_2.1.1/
 
 VERSION_ID=`cat /etc/os-release | grep "VERSION_ID" | cut -d '"' -f 2`
-echo $VERSION_ID
+echo ${VERSION_ID}
+
+BASE_URL=https://raw.githubusercontent.com/FREEWING-JP/phantomjs/feature/build_raspberry_pi_2.1.1/raspberry_pi_2.1.1/
 
 TAR_FILE=""
 if [ "${VERSION_ID}" == '8' ]; then
@@ -16,9 +16,12 @@ elif [ "${VERSION_ID}" == '9' ]; then
 else
   echo "not supported os version ."
 fi
+
 if [ -n "${TAR_FILE}" ]; then
-  echo $TAR_FILE
-  tar xvf $TAR_FILE
+  echo ${TAR_FILE}
+  wget ${BASE_URL}${TAR_FILE}
+  tar xvf ${TAR_FILE}
 fi
+
 ls -l phantomjs
 ./phantomjs -v
